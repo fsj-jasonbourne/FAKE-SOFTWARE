d487aaa49fd143bf96429e2484cf82814dfb4ddf
fushijia <876228013@qq.com>
2016-09-08 16:54:07 +0800

    modified:   fake_package.sh

--- fake_package.sh
+++ fake_package.sh
@@ -16,53 +16,53 @@ specs_name="rpmtest.spec"
 
 function build_path()
 {
-	if [ ! -d ${build_dir} ] 
-	then
+    if [ ! -d ${build_dir} ]  
+    then
     {
-		mkdir ${build_dir} 
-		echo "创建${build_dir}目录成功"
-	}	   
+        mkdir ${build_dir} 
+        echo "创建${build_dir}目录成功"
+    }	   
     fi
 
     if [ ! -d ${buildroot_dir} ]
-	then
-	{
-		mkdir ${buildroot_dir} 
-		echo "创建${buildroot_dir}目录成功"
-	}
+    then
+    {
+	mkdir ${buildroot_dir} 
+	echo "创建${buildroot_dir}目录成功"
+    }
     fi
 
     if [ ! -d ${rpm_dir} ]
-	then
-	{
-		mkdir ${rpm_dir} 
-		echo "创建${rpm_dir}目录成功"
-	}
+    then
+    {
+	mkdir ${rpm_dir} 
+	echo "创建${rpm_dir}目录成功"
+    }
     fi
 
     if [ ! -d ${source_dir} ]
-	then
-	{
-		mkdir ${source_dir} 
-		echo "创建${source_dir}目录成功"
-	}
+    then
+    {
+	mkdir ${source_dir} 
+	echo "创建${source_dir}目录成功"
+    }
     fi
 
     if [ ! -d ${specs_dir} ]
-	then
-	{
-		mkdir ${specs_dir} 
-		echo "创建${specs_dir}目录成功"
-	}
+    then
+    {
+	mkdir ${specs_dir} 
+	echo "创建${specs_dir}目录成功"
+    }
     fi
 
     if [ ! -d ${sprms_dir} ]
-	then
-	{
-		mkdir ${sprms_dir} 
-		echo "创建${sprms_dir}目录成功"
-	}
-	fi
+    then
+    {
+	mkdir ${sprms_dir} 
+	echo "创建${sprms_dir}目录成功"
+    }
+    fi
 
 }
 
@@ -74,64 +74,64 @@ function fake_pack()
 
 function fake_number()
 {
-	echo "Please input your package name:"
-	read fake_name
-	echo "Please input number of your package:"
-	read fake_number
-	for ((i=1;i<${fake_number};i++))
-	do
-	{
-	   # 设置包名称 packname+packnumber+packnumber
-       sed -i "/Name:.*/s/:.*/:${fake_name}${i}/g" ${specs_name}
-	   echo "${fake_name}${i}${i}"
-	   # 调用fake_pack函数
-       fake_pack
-	}
+    echo "Please input your package name:"
+    read fake_name
+    echo "Please input number of your package:"
+    read fake_number
+    for ((i=1;i<${fake_number};i++))
+    do
+    {
+        # 设置包名称 packname+packnumber+packnumber
+        sed -i "/Name:.*/s/:.*/:${fake_name}${i}/g" ${specs_name}
+	echo "${fake_name}${i}${i}"
+	# 调用fake_pack函数
+        fake_pack
+    }
     done
 }
 
 function move_package()
 {
-	mv ${rpm_dir}/* ${move_dir}
+    mv ${rpm_dir}/* ${move_dir}
 }
 
 function install_pack()
 {
     software=`ls ${move_dir}/i386/`    
-	for soft in ${software}
+    for soft in ${software}
     do
-		rpm -ivh ${move_dir}/i386/${soft}
-		sleep 2
-	done
+	rpm -ivh ${move_dir}/i386/${soft}
+	sleep 2
+    done
 }
 
 function uninstall_pack()
 {
     software=`ls ${move_dir}/i386/`    
-	for soft in ${software}
+    for soft in ${software}
     do
-		softname=`echo "${soft}" |awk -F '-' '{print $1}'`
-		#echo "${softname}"
-		rpm -e ${softname}
-		sleep 2
-	done
+	softname=`echo "${soft}" |awk -F '-' '{print $1}'`
+	#echo "${softname}"
+	rpm -e ${softname}
+	sleep 2
+    done
 }
 
 
 function Main()
 {
-	#检验打包路径
+    #检验打包路径
     build_path
-	echo "build_path done"
-	#设定打包名称及数量
+    echo "build_path done"
+    #设定打包名称及数量
     fake_number
-	echo "fake_number done"
-	#打包后进行移动
+    echo "fake_number done"
+    #打包后进行移动
     move_package
-	echo "move_package done"
+    echo "move_package done"
     install_pack
-	#移动完成后进行安装
-	echo "install_pack done"
+    #移动完成后进行安装
+    echo "install_pack done"
     #uninstall_pack
 }
 
